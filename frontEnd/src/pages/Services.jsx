8bb7e6bb73e5de0692f4d78780ad935c6edd94c0
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-06-23 10:43:22 +0530

    services page complete responsiveness done - 100%

--- frontEnd/src/pages/Services.jsx
+++ frontEnd/src/pages/Services.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import ServiceCard from "../components/Services/ServiceCard";
+import { services } from "../assets/data/services";
 
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <div className="container">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+        {services.map((item, index) => (
+          <ServiceCard key={index} item={item} index={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
